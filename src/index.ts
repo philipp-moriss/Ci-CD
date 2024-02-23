828269e45f3dc6b847a6ac7cf0d44d7bdb4a4422
Philip Veremeichik <p.veremeichik@richbrains.net>
2024-02-23 10:39:51 +0300

    first commit

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import dotenv from "dotenv";
+import express, { Request, Response } from "express";
+import { sum } from "./sum/sum";
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT;
+
+app.get("/", (req: Request, res: Response) => {
+  const sumResult = sum(10, 10);
+  res.send(`Hello World + ${sumResult}`);
+});
+
+app.listen(port, () => {
+  console.log(`[server]: Server is running at http://localhost:${port}`);
+});
